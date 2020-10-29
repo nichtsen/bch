c0af61ff4669b97bc72b4e1ddce9c63b6331dd3c
nichtsen <hobblong@163.com>
2020-10-29 10:45:32 +0800

    my branch change main func

--- main.go
+++ main.go
@@ -3,20 +3,20 @@ package main
 import "fmt"
 
 func init() {
-    fmt.Println("dev add init()")
+	fmt.Println("dev add init()")
 }
 
 func main() {
-    fmt.Println("main")
-    
-    fmt.Println("dev change main")
+	fmt.Println("main")
+
+	fmt.Println("my branch change main")
 }
 
 func foo() {
-    fmt.Println("foo")
+	fmt.Println("foo")
 }
 
 func bar() {
-    fmt.Println("bar")
-    fmt.Println("dev chang bar function")
+	fmt.Println("bar")
+	fmt.Println("dev chang bar function")
 }
