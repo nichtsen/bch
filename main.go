9afc04a81bbed984b843a9f35340a99fbac5f8cb
nichtsen <hobblong@163.com>
2020-10-29 11:04:37 +0800

    msg

--- main.go
+++ main.go
@@ -8,14 +8,7 @@ func init() {
 
 func main() {
 	fmt.Println("main")
-<<<<<<< HEAD
-	fmt.Pritnln("main branch change main func")
 
-	fmt.Println("dev change main")
-=======
-
-	fmt.Println("my branch change main")
->>>>>>> my
 }
 
 func foo() {
@@ -24,5 +17,4 @@ func foo() {
 
 func bar() {
 	fmt.Println("bar")
-	fmt.Println("dev chang bar function")
 }
